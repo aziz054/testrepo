dc221c2d88e3398e1474cf7d43bcb3cd566f53fc
Abdul Aziz <144003953+aziz054@users.noreply.github.com>
2023-09-06 01:35:59 +0500

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science tools and ecosystem
+# 
+
+#  ## Exercise 3
+#  In this notebook, Data Science Tools and Ecosystem are summarized.
+#  
+
+# ## Exercise 10 - Insert a markdown cell to list Objectives
+# ### Solution:
+#  #### Objectives:
+#  - Python
+#  * R 
+#  + Sql
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R 
+# 3. SQL
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. PyTorch
+# 2. Tensorflow
+# 3. caffe
+
+# ### Exercise 6 - Create a markdown cell with a table of Data Science tools
+# 
+# | Data Science Tools |
+# |--------------------|
+# |RStudio|
+# |Apache Spark|
+# |Apache Hadoop|
+
+# # Exercise 7 - Create a markdown cell introducing arithmetic expression examples
+# 
+# ## Solution:
+# 
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# ## Exercise 8 - Create a code cell to multiply and add numbers
+# ### Solution:
+# 
+# 
+
+# In[4]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+print((3*4)+5) 
+
+
+# ## Exercise 9 - Create a code cell to convert minutes to hours.
+# ### Solution:
+
+# In[5]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+print(200/60)
+
+
+# ## Exercise 11 - Create a markdown cell to indicate the Author's name
+# 
+# ### Solution:
+# ## Author 
+# Abdul Aziz
+# 
+
+# In[ ]:
+
+
+
+
